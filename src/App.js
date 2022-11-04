1399790178295598d1a521ab66bb243ce262a5b2
ChristianArielCostaLiloff <private.c.costa@gmail.com>
2022-11-04 12:43:43 -0300

    Home layout added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./layouts/Home";
 
 function App() {
   return (
-    <div>Hola Mundo somos el grupo de LeeroyJenkins</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
