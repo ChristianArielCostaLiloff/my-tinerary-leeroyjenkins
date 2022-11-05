21bdd4b2af0f5a536b13e70daad3a7f5898a597e
ChristianArielCostaLiloff <private.c.costa@gmail.com>
2022-11-05 14:58:16 -0300

    Added new styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./components/Home";
 import NotFound from "./components/NotFound";
 import Layout from "./layouts/Layout";
+import SingUp from "./pages/SingUp";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="*" element={<NotFound />} />
+          <Route path="/singup" element={<SingUp/>}></Route>
         </Routes>
       </Layout>
     </BrowserRouter>
