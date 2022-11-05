c1b2a5ff9edac37f1725daa7bd595fb8c52edc4b
ChristianArielCostaLiloff <private.c.costa@gmail.com>
2022-11-04 21:06:17 -0300

    Implemented Layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./layouts/Home";
+import Home from "./components/Home";
+import Layout from "./layouts/Layout";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
