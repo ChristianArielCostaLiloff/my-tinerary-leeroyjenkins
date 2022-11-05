0aa9a183ef8c2de4e3d1a18d061b501a91fc9c77
Hugo Smahlij <smahlij@gmail.com>
2022-11-05 17:34:30 -0300

    fixed nameClass error

--- src/components/GoogleButton.jsx
+++ src/components/GoogleButton.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function GoogleButton(props) {
   return (
     <div className="gSignInWrapper">
-      <div id="customBtn" class="customGPlusSignIn">
+      <div id="customBtn" className="customGPlusSignIn">
         <span className="icon"></span>
         <span className="buttonText">{props.children}</span>
       </div>
