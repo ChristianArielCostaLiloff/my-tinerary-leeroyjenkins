f257273d6ece35719dd8847c72b77e40ed022012
Hugo Smahlij <smahlij@gmail.com>
2022-11-06 20:00:37 -0300

    added hotels page component, style for it and a route in app.js

---
+++ src/pages/Hotels.jsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react'
+import Card from '../components/Card'
+import SearchBar from '../components/SearchBar'
+import { hotel as hotelsData } from "../data/hotels"
+import NoElementsFound from '../components/NoElementsFound'
+import { json } from 'react-router-dom'
+
+export default function Hotels() {
+    let [hotel, setHotel] = useState(hotelsData);
+
+    useEffect(() => {
+        setHotel(hotel.map(e => {
+            e.photo = e.photo[0]
+            return e
+        }))
+        let mainNavBar = document.getElementById("hotels-nav");
+        mainNavBar.addEventListener("input", showSortedCard);
+    }, []);
+
+    let showSortedCard = () => {
+        let searchText = document.getElementById("search-text").value.toLowerCase();
+        let selectSort = document.getElementById("format").value.toLowerCase();
+        if (selectSort === 'low') {
+            hotel.sort((a, b) => a.capacity - b.capacity)
+        }
+        if (selectSort === 'high') {
+            hotel.sort((a, b) => b.capacity - a.capacity)
+        }
+        let sortedHotels = hotel.filter(hotel => hotel.name.toLowerCase().includes(searchText))
+        setHotel(sortedHotels)
+        console.log("Filtered by: SortedPer: " + selectSort + " Text: " + searchText);
+        localStorage.setItem("filterHotel", JSON.stringify({ text: searchText, sortPer: selectSort }))
+    };
+
+
+
+
+    return (
+        <>
+            <div className="base-cities">
+                <div className="cities-nav" id="hotels-nav">
+                    <div className="select">
+                        <select name="format" id="format">
+                            <option defaultValue>Order by capacity</option>
+                            <option value="high">From high to low</option>
+                            <option value="low">From low to high</option>
+                        </select>
+                    </div>
+                    <SearchBar />
+                </div>
+                <div className="card-container">
+                    {hotel.length > 0 ? (
+
+                        hotel.map((hotel) => {
+                            /* hotel.photo = hotel.photo[0] */
+                            return (
+                                <Card city={hotel} key={hotel.id}>
+                                    Capacity: {hotel.capacity}
+                                </Card>)
+                        })
+                    ) : (
+                        <NoElementsFound />
+                    )}
+                </div>
+            </div>
+        </>
+    )
+}
