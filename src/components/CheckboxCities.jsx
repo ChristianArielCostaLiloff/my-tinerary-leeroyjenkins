5b4efff965ff4c570f8fe84e6960ec6488034f9d
ChristianArielCostaLiloff <private.c.costa@gmail.com>
2022-11-06 13:35:48 -0300

    Deleted innecesary clg

--- src/components/CheckboxCities.jsx
+++ src/components/CheckboxCities.jsx
@@ -3,8 +3,6 @@ import { cities } from "../data/cities";
 
 export default function CheckboxCities(props) {
   let { continent } = props;
-  
-  console.log(continent);
   return (
     <>
       <input type="checkbox" name="continent" id={continent.toLowerCase()} value={continent.toLowerCase()} />
