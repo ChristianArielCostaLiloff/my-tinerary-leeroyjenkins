a977f2ba32d360eb553b96bd825ac8b21a278dba
ChristianArielCostaLiloff <private.c.costa@gmail.com>
2022-11-13 02:41:27 -0300

    Added onChange to SearchBar component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 
-export default function SearchBar() {
+export default function SearchBar(props) {
+  let { reference , onChange } = props;
   return (
     <div className="container_searchbox">
-      <form action="" className="search_bar">
-        <input type="text" placeholder="Search.." name="q" id="search-text" />
+      <div className="search_bar">
+        <input type="text" placeholder="Search.." name="q" id="search-text" ref={reference} onChange={onChange}/>
         <button type="button">
           <img src="./img/search.png" />
         </button>
-      </form>
+      </div>
     </div>
   );
 }
