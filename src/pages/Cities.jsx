1379e137b0a70097a01cbf385ab3daf354f00ca0
ChristianArielCostaLiloff <private.c.costa@gmail.com>
2022-11-21 01:51:40 -0300

    Used redux to get cities and filters

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -1,65 +1,60 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Card from "../components/Card";
 import CheckboxCities from "../components/CheckboxCities";
 import NoElementsFound from "../components/NoElementsFound";
 import SearchBar from "../components/SearchBar";
-import apiUrl from "../url";
+import cityActions from "../redux/actions/cityActions";
 
 export default function Cities() {
-  let searchBar = useRef();
-  let [cities, setCities] = useState([]);
-  let [citiesData, setCitiesData] = useState([])
-  let continents;
+  const checkboxRef = useRef([]);
+  const searchRef = useRef();
+  let cities = useSelector((store) => store.cityReducer.cities);
+  let continents = useSelector((store) => store.cityReducer.continent);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    axios
-      .get(`${apiUrl}/api/city`)
-      .then((res) => {
-        setCities(res.data.response);
-        setCitiesData(res.data.response);
-      })
-      .catch((error) => console.log(error));
+    dispatch(cityActions.getCities());
   }, []);
 
-  continents = [...new Set(citiesData.map((city) => city.continent))];
-
-  let sortCard = () => {
-    let continentsCheck = Array.from(
-      document.querySelectorAll("input[name='continent']:checked")
-    )
-      .map((node) => "continent=" + node.value)
-      .join("&");
-
-    axios
-      .get(
-        `${apiUrl}/api/city?${continentsCheck}&name=${searchBar.current.value}`
-      )
-      .then((res) => setCities(res.data.response));
+  const handleChange = () => {
+    let filters = {
+      continent: checkboxRef.current
+        .filter((checkbox) => checkbox.checked)
+        .map((checkbox) => checkbox.value),
+      name: searchRef.current.value,
+    };
+    dispatch(cityActions.getCitiesByNameAndContinent(filters));
   };
 
   return (
     <div className="base-cities">
       <div className="cities-nav" id="cities-nav">
-        <form className="nav_form" onChange={sortCard}>
+        <form className="nav_form" onChange={handleChange}>
           <div className="container_checkbox">
             <div className="form_checkbox">
               <div className="checkboxes">
                 <h2>Search by continent</h2>
                 {continents.map((continent, index) => (
-                  <CheckboxCities key={index} continent={continent} onChange={sortCard} />
+                  <CheckboxCities
+                    key={index}
+                    continent={continent}
+                    reference={(checkbox) =>
+                      (checkboxRef.current[index] = checkbox)
+                    }
+                  />
                 ))}
               </div>
             </div>
           </div>
-          <SearchBar reference={searchBar} />
+          <SearchBar reference={searchRef} />
         </form>
       </div>
       <div className="card-container" id="container-card">
         {cities.length > 0 ? (
           cities.map((city) => (
-            <Card type='city' element={city} key={city._id}>
+            <Card type="city" element={city} key={city._id}>
               Population: {city.population}
             </Card>
           ))
