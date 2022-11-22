dee62d708835de5cad6cdb63996b56fe887f068f
Hugo Smahlij <smahlij@gmail.com>
2022-11-22 02:48:30 -0300

    changed newHotel code to create a hotel

--- src/pages/NewHotel.jsx
+++ src/pages/NewHotel.jsx
@@ -2,24 +2,56 @@ import React from 'react'
 import InputFormSign from '../components/InputFormSign'
 import apiUrl from '../url';
 import axios from 'axios';
+import { useRef } from 'react';
+import Swal from 'sweetalert2';
 
 export default function NewHotel() {
-    let newHotel = () => {
+
+      const name = useRef(null);
+      const photo = useRef(null);
+      const capacity = useRef(null);
+    
+      let handleSubmit = () => {
         let newHotel = {
-          name: document.getElementById("name").value,
-          photo: document.getElementById("photo").value,
-          capacity: document.getElementById("capacity").value,
-          cityId: "63701f25d10c25267b79e295",
+          name: name.current.value,
+          photo: photo.current.value,
+          capacity: capacity.current.value,
+          cityId: "63701f25d10c25267b79e294",
           userId: "6370096b26cecde13c02e04c",
         };
         try {
-          axios.post(`${apiUrl}/api/hotel`, newHotel);
-          alert("Hotel created in BD");
+          Swal.fire({
+            title: "Do you want create this hotel?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#006466",
+            cancelButtonColor: "#212F45",
+            confirmButtonText: "Create hotel",
+          }).then(async (result) => {
+            if (result.isConfirmed) {
+              let res = await axios.post(`${apiUrl}/api/hotel`, newHotel);
+              console.log(res);
+              if (res.data.success) {
+                Swal.fire("Builded!", "Your hotel has been created", "success").then(
+                  (result) => {
+                    if (result.isConfirmed) {
+                      window.location.href =`/details/hotel/${res.data.id}`;
+                    }
+                  }
+                );
+              } else {
+                Swal.fire(
+                  "Something went wrong!",
+                  res.data.message.join("<br>"),
+                  "error"
+                );
+              }
+            }
+          });
         } catch (error) {
           alert("Error with axios");
           console.log(error.message);
         }
-        window.location.reload(true);
       };
     return (
         <div className="body new-hotel">
@@ -28,17 +60,17 @@ export default function NewHotel() {
                 <div className="content">
                     <form>
                         <div className="user-details">
-                            <InputFormSign type="text" id="name" text="Enter hotel name">
+                            <InputFormSign type="text" reference={name} text="Enter hotel name">
                                 Hotel name
                             </InputFormSign>
-                            <InputFormSign type="text" id="photo" text="Enter photo url">
+                            <InputFormSign type="text" reference={photo} text="Enter photo url">
                                 Photo URL
                             </InputFormSign>
                             <div className="input-box">
                                 <span className="details">Capacity</span>
                                 <input
                                     type="number"
-                                    id="capacity"
+                                    ref={capacity}
                                     placeholder="Enter capacity"
                                     required
                                     min="0"
@@ -48,7 +80,7 @@ export default function NewHotel() {
                         <div className="button">
                             <input
                                 type="button"
-                                onClick={newHotel}
+                                onClick={handleSubmit}
                                 value="Register new hotel"
                             />
                         </div>
